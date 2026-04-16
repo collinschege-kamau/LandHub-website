c6cb8cc61e9e7c439ce07a2476942faeffcf6bd5
collinschege-kamau <collokrymboy@gmail.com>
2026-04-16 08:49:18 +0300

    Update delete_listing.php

--- delete_listing.php
+++ delete_listing.php
@@ -1,11 +1,23 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
+
+// Database Connection
+$servername="sql112.infinityfree.com";
+$db_username="if0_41669716";
+$db_password="v625mgR7min";
+$dbname="if0_41669716_landapp";
+
+$conn = new mysqli($servername, $db_username, $db_password, $dbname);
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 // Ensure only the logged-in company can delete
 if(!isset($_SESSION['username']) || $_SESSION['role'] !== 'seller'){
     exit("Unauthorized");
 }
-
-$conn = new mysqli("localhost", "root", "", "land app");
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
