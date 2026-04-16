d3242439311fdf20cdc24ced64061e9499ebed27
collinschege-kamau <collokrymboy@gmail.com>
2026-04-16 16:56:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -295,7 +295,7 @@
                         <i class="fa fa-envelope" style="color: var(--accent);"></i> collokrymboy@gmail.com
                     </div>
                     <div class="contact-item">
-                        <i class="fa fa-phone" style="color: var(--success);"></i> +254 715 185922
+                        <br><i class="fa fa-phone" style="color: var(--success);"></i> +254 715 185922
                     </div>
                 </div>
             </section>
