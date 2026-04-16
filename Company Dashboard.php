e081a750f102feed493cb1c4c46ff467cc7e901b
collinschege-kamau <collokrymboy@gmail.com>
2026-04-16 17:25:39 +0300

    Update Company Dashboard.php

--- Company Dashboard.php
+++ Company Dashboard.php
@@ -172,6 +172,7 @@ if($loggedInCompanyId){
 <head>
     <meta charset="UTF-8">
     <title>Seller Dashboard | LandHub</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     
