68fd687dc4c801bdb7b5b117bddad3b581dc0e66
collinschege-kamau <collokrymboy@gmail.com>
2026-04-26 15:48:41 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const cacheName = 'landhub-v1';
+const assets = [
+  '/',
+  '/index.php',
+  '/listings.php', // Add your other important pages here
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'
+];
+
+// This runs when the app is first installed
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('LandHub: Caching system files');
+      return cache.addAll(assets);
+    })
+  );
+});
