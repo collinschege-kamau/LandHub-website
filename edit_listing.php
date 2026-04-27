17dab5db3aa61473e09814a37ace0f4225f32ccc
collinschege-kamau <collokrymboy@gmail.com>
2026-04-27 17:24:28 +0300

    Update edit_listing.php

--- edit_listing.php
+++ edit_listing.php
@@ -5,15 +5,8 @@ error_reporting(E_ALL);
 session_start();
 
 // Database Connection
-$servername="sql112.infinityfree.com";
-$db_username="if0_41669716";
-$db_password="v625mgR7min";
-$dbname="if0_41669716_landapp";
+require_once 'config.php';
 
-$conn = new mysqli($servername, $db_username, $db_password, $dbname);
-if($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 // Security Check - Ensure 'seller' role matches your login system
 if(!isset($_SESSION['user_id'])){
     header("Location: Login.php");
