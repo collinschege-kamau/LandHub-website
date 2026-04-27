d9fec1a8ffc5ddf683bd3b4d1432d4d648656777
collinschege-kamau <collokrymboy@gmail.com>
2026-04-27 17:26:02 +0300

    Update reset_new_password.php

--- reset_new_password.php
+++ reset_new_password.php
@@ -3,12 +3,7 @@ date_default_timezone_set('Africa/Nairobi');
 session_start();
 
 // Database Connection
-$servername="sql112.infinityfree.com";
-$db_username="if0_41669716";
-$db_password="v625mgR7min";
-$dbname="if0_41669716_landapp";
-
-$conn=new mysqli($servername,$db_username,$db_password,$dbname);
+require_once 'config.php';
 
 // Check connection
 if ($conn->connect_error) {
